0e8aaf6286f05f9349aa070fe42b9d6c69b95bdc
jparker <me@jasonparker.info>
2025-10-12 01:12:56 +0000

    remove unused import

--- src/main/java/com/adventofcode/y2024/Day9.java
+++ src/main/java/com/adventofcode/y2024/Day9.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Pattern;
+
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
