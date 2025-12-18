57113fde7b7b9d74b47923e1ee48e85f3afbadea
j.parker <jparker@jasonparker.info>
2025-12-18 22:59:43 +0000

    rename method to clarify intent

--- src/main/java/com/adventofcode/util/PathUtil.java
+++ src/main/java/com/adventofcode/util/PathUtil.java
@@ -9,17 +9,19 @@ public class PathUtil {
 
     private static final Path resourceDir = Path.of("src", "test", "resources");
     
-    public static Path getExampleData(Year year, Day day) {
+    public static Path getExampleDataPath(Year year, Day day) {
         return Path.of(PathUtil.resourceDir.toString(), year.getYear(), "examples", day.getDay());
     }
 
-    public static Path getChallengeData(Year year, Day day) {
+    public static Path getChallengeDataPath(Year year, Day day) {
         return Path.of(PathUtil.resourceDir.toString(), year.getYear(), "challenges", day.getDay());
     }
     
     public static List<String> getChallengeLines(Year year, Day day) {
         try {
-            return Files.readAllLines(getChallengeData(year, day));
+            return Files.lines(getChallengeDataPath(year, day))
+                .filter(l -> !l.isBlank())
+                .toList();
         } catch (IOException ex) {
             throw new IllegalArgumentException("you suck!", ex);
         }
@@ -27,7 +29,9 @@ public class PathUtil {
 
     public static List<String> getExampleLines(Year year, Day day) {
         try {
-            return Files.readAllLines(getExampleData(year, day));
+            return Files.lines(getExampleDataPath(year, day))
+                .filter(l -> !l.isBlank())
+                .toList();
         } catch (IOException ex) {
             throw new IllegalArgumentException("you suck!", ex);
         }
